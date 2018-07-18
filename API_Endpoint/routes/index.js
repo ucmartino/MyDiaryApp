eee6a4f3a3608b4f224963c4cf065f4e1c7df1fc
Martiniyooh@gmail.com <Martiniyooh@gmail.com>
2018-07-18 20:45:35 +0100

    Restructure my routes file

--- API_Endpoint/routes/index.js
+++ API_Endpoint/routes/index.js
@@ -1,73 +1,10 @@
-const express = require("express");
-const router = express.Router();
+import Diaries from '../controller/diary';
+import diaries from '../model/diary';
 
-//LANDING PAGE 
-router.get("/", (req, res) => {
-    res.send("Welcome to MyDiary App");
- });
- 
- //SHOW ALL DIARY ENTRIES
- router.get("/api/v1/diaries", (req, res) => {
-     return res.json({
-         diaries
-     });
- });
- 
- //POST ROUTE
- router.post("/api/v1/diaries", (req, res) => {
-     const id = diaries.length + 1;
-     const title = req.body.title;
-     const content = req.body.content;
-     const newDiary = {id: id, title: title, content: content};
-     diaries.push(newDiary);
-     res.json([diaries]);
- });
-
- //SHOW ROUTE FOR SINGLE DIARY ENTRY
- router.get("/api/v1/diaries/:diaryId", (req, res) => {
-     for (let i = 0; i < diaries.length; i++ ) {
-         if (diaries[i].id === parseInt(req.params.diaryId)) {
-           return res.json({
-             diaries: diaries[i],
-             message: 'success',
-           });
-         }
-       }
-       return res.status(404).json({
-         message: 'diary not found',
-       });
- });
- 
- //UPDATE ROUTE
- router.put("/api/v1/diaries/:diaryId", (req, res) => {
-     for (let i = 0; i < diaries.length; i++) {
-         if (diaries[i].id === parseInt(req.params.diaryId)) {
-           diaries[i].title = req.body.title;
-           diaries[i].content = req.body.content;
-           return res.json({
-             diaries,
-             message: 'diary updated successfully',
-           });
-         }
-       }
-       return res.status(404).json({
-         message: 'diary not found',
-       });
- });
- 
- //DELETE ROUTE
- router.delete("/api/v1/diaries/:diaryId", (req, res) => {
-     for (let i = 0; i < diaries.length; i += 1) {
-         if (diaries[i].id === parseInt(req.params.diaryId)) {
-           diaries.splice(i, 1);
-           return res.json({
-             message: 'diary removed successfully',
-           });
-         }
-       }
-       return res.status(404).json({
-         message: 'diary not found',
-       });
- });
-
- module.exports = router;
+export default (app) => {
+  app.get('/api/v1/diaries', Diaries.getDiary);
+  app.post('/api/v1/diaries', Diaries.createDiaries);
+  app.put('/api/diaries/:diaryId', Diaries.updateDcreateDiaries);
+  app.delete('/api/diaries/:diaryId', Diaries.removeDcreateDiaries);
+  app.get('/api/diaries/:diaryId', Diaries.retrieveDcreateDiaries);
+};
