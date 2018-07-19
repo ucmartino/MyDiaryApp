550feae0be5772c54e613135d3affac3d947044e
Martiniyooh@gmail.com <Martiniyooh@gmail.com>
2018-07-19 19:59:58 +0100

    Structure the route term appropriately

--- API_Endpoint/routes/index.js
+++ API_Endpoint/routes/index.js
@@ -2,9 +2,9 @@ const Diaries = require('../controller/diary');
 const diaries = require('../model/diary');
 
 module.exports = (app) => {
-  app.get('/api/v1/diaries', Diaries.getDiary);
-  app.get('/api/v1/diaries/:diaryId', Diaries.retrieveDiaries);
-  app.post('/api/v1/diaries', Diaries.createDiaries);
-  app.put('/api/v1/diaries/:diaryId', Diaries.updateDiaries);
-  app.delete('/api/v1/diaries/:diaryId', Diaries.removeDiaries);
+  app.get('/api/v1/diaries', Diaries.getDiaries);
+  app.get('/api/v1/diaries/:diaryId', Diaries.retrieveDiary);
+  app.post('/api/v1/diaries', Diaries.createDiary);
+  app.put('/api/v1/diaries/:diaryId', Diaries.updateDiary);
+  app.delete('/api/v1/diaries/:diaryId', Diaries.removeDiary);
 };
