c44c21340930cdd7cf6fce56d644cdf04a8b1d23
Martiniyooh@gmail.com <Martiniyooh@gmail.com>
2018-07-19 19:45:08 +0100

    Refactor the routes file to contain only the API endpoints

--- API_Endpoint/routes/index.js
+++ API_Endpoint/routes/index.js
@@ -1,75 +1,10 @@
-const express = require("express");
-const router = express.Router();
-const diaries = require("../model/diary");
+const Diaries = require('../controller/diary');
+const diaries = require('../model/diary');
 
-//LANDING PAGE 
-router.get("/", (req, res) => {
-  res.send("Welcome to MyDiary App");
-});
- 
-//SHOW ALL DIARY ENTRIES
-router.get("/api/v1/diaries", (req, res) => {
-  return res.json({ diaries });
-});
- 
-//POST ROUTE
-router.post("/api/v1/diaries", (req, res) => {
-  diaries.push({
-    id: diaries.length + 1,
-    title: req.body.title,
-    content: req.body.content
-  });
-  return res.json({
-    diaries,
-    message: 'new diary created successfully'
-  });
-});
-
-//SHOW ROUTE FOR SINGLE DIARY ENTRY
-router.get("/api/v1/diaries/:diaryId", (req, res) => {
-  for (let i = 0; i < diaries.length; i++) {
-    if (diaries[i].id === parseInt(req.params.diaryId)) {
-      return res.json({
-        diaries: diaries[i],
-        message: 'success'
-      });
-    }
-  }
-  return res.status(404).json({
-    message: 'diary not found'
-  });
-});
- 
-//UPDATE ROUTE
-router.put("/api/v1/diaries/:diaryId", (req, res) => {
-  for (let i = 0; i < diaries.length; i++) {
-    if (diaries[i].id === parseInt(req.params.diaryId)) {
-      diaries[i].title = req.body.title;
-      diaries[i].content = req.body.content;
-      return res.json({
-        diaries,
-        message: 'diary updated successfully'
-      });
-    }
-  }
-  return res.status(404).json({
-    message: 'diary not found'
-  });
-});
- 
-//DELETE ROUTE
-router.delete("/api/v1/diaries/:diaryId", (req, res) => {
-  for (let i = 0; i < diaries.length; i += 1) {
-    if (diaries[i].id === parseInt(req.params.diaryId)) {
-      diaries.splice(i, 1);
-      return res.json({
-        message: 'diary removed successfully'
-      });
-    }
-  }
-  return res.status(404).json({
-    message: 'diary not found'
-  });
-});
-
-module.exports = router;
+module.exports = (app) => {
+  app.get('/api/v1/diaries', Diaries.getDiary);
+  app.get('/api/v1/diaries/:diaryId', Diaries.retrieveDiaries);
+  app.post('/api/v1/diaries', Diaries.createDiaries);
+  app.put('/api/v1/diaries/:diaryId', Diaries.updateDiaries);
+  app.delete('/api/v1/diaries/:diaryId', Diaries.removeDiaries);
+};
